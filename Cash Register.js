577f3ebc8a64b2f68382d04e6e0cb20941fdfa25
legenddaniel <siyuanincanada@gmail.com>
2019-12-11 18:31:16 -0500

    Add some comments

--- Cash Register.js
+++ Cash Register.js
@@ -17,21 +17,19 @@ const checkCashRegister = (price, cash, cid) => {
     const balance = cashCal(cid);
     cid.reverse();
 
+    // First we count how much we need for this face value of cash, then take all/part of a specific face value of cash out of the cid and set it as the change, finally calculate how much left for the calculation of next face value of cash. *100/100 for rounding issue
     for (let unit = 0; unit < cid.length; unit++) {
-        // need: how much we need for this face value of cash
         const need = currency[cid[unit][0]] * Math.floor(changeDue / currency[cid[unit][0]]);
-        // take all/part of a specific face value of cash out of the cid and set it as the change
         cacheChange[unit][1] = (need <= cid[unit][1]) ? need : cid[unit][1];
-        // how much left for the calculation of next face value of cash, *100/100 for rounding issue
         changeDue = Math.round((changeDue - cid[unit][1]) * 100) / 100;
     }
 
+    // "CLOSED": number of change is exactly the same as the balance in the cash register
+    // "insufficient funds": either we don't have enough money, or we only have cash whose face value is greater than the change
     if (!changeDue && cashCal(cacheChange) === balance) {
-        // number of change is exactly the same as the balance in the cash register
         status = "CLOSED";
         change = cid.reverse();
     } else if (changeDue) {
-        // This "insufficient funds" means either we don't have enough money or we only have cash whose face value is greater than the change
         status = "INSUFFICIENT_FUNDS";
         change = [];
     } else {
