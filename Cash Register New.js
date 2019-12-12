2ceb1e79043e7c1024a291261aeb19e0dead4d20
legenddaniel <siyuanincanada@gmail.com>
2019-12-11 21:10:23 -0500

    Update Cash Register New.js

--- Cash Register New.js
+++ Cash Register New.js
@@ -28,32 +28,21 @@ const checkCashRegister = (price, cash, cid) => {
     // }
 
     let [cacheChange, status, changeDue, change] = [cid, '', cash - price, []];
-    const cashCal = money => money.flat().filter(a => typeof a === 'number').reduce((a, b) => a + b);
-    const balance = cashCal(cid);
-    cid.reverse();
+    // const cashCal = money => money.flat().filter(a => typeof a === 'number').reduce((a, b) => a + b);
+    cid.reverse(); //Ready to remove
 
     // First we count how many we need for this face value of cash, then take all/part of a specific face value of cash out of the cid and set it as the change, finally calculate how much left for the calculation of next face value of cash. *100/100 for rounding issue
     for (let unit = 0; unit < cid.length; unit++) {
         const need = Math.floor(changeDue / currency[cid[unit][0]]);
-        cacheChange[unit][1] = (need <= cid[unit][1]) ? need : cid[unit][1];
+        cacheChange[unit][1] = Math.min(need, cid[unit][1]);
         changeDue = Math.round((changeDue - cacheChange[unit][1] * currency[cid[unit][0]]) * 100) / 100;
     }
 
-    // "CLOSED": number of change is exactly the same as the balance in the cash register
-    // "insufficient funds": either we don't have enough money, or we only have cash whose face value is greater than the change
-    if (!changeDue && cashCal(cacheChange) === balance) {
-        status = "CLOSED";
-        change = cid.reverse();
-    } else if (changeDue) {
-        status = "INSUFFICIENT_FUNDS";
-        change = [];
-    } else {
-        status = "OPEN";
-        // remove the face value which is not used
-        change = cacheChange.filter(unit => unit[1] !== 0);
-    }
-
-    return { 'status': status, 'change': change };
+    // "Insufficient funds": either we don't have enough money, or we only have cash whose face value is greater than the change
+    return (
+        [status, change] = changeDue ? ["INSUFFICIENT_FUNDS", []] : ["OPEN", cacheChange.filter(unit => unit[1] !== 0)],
+        {'status': status,'change': change}
+    );
 }
 
 // Numbers in the cid mean amount of each face value of coin/cash
