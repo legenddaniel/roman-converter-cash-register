bd4ca87ac361464bd07e4f00fff87907d76c4b2f
legenddaniel <siyuanincanada@gmail.com>
2019-12-17 22:58:32 -0500

    1

--- About.js
+++ About.js
@@ -1,18 +1,46 @@
 $(() => {
-    const loading = setInterval(fnLoading, interval);
-    const interval = Math.random() * 1000;
-    const step = Math.floor(math.random() * 11) + 10;
+    // const interval = () => Math.random() * 1000;
+    // let progress = 0;
+    // const fnLoading = () => {
+    //     $('.progress-bar').css('width', `${progress}%`);
+    //     $('.progress span').text(`${progress}%`);
+    //     const fnLoaded = () => {
+    //         clearInterval(loading);
+    //         $('.progress-bar').css('width', '100%');
+    //         $('.progress span').text('100%');
+    //         return setTimeout(() => {
+    //             $('.progress span').addClass('text-warning bg-info px-2');
+    //             $('.progress span').css('font-size', '1rem');
+    //             $('.progress span').text('Click either tab to start');
+    //         }, 3000)
+    //     }
+    //     const step = () => Math.floor(Math.random() * 11) + 10;
+    //     progress >= 100 ? fnLoaded() : progress += step();
+    // }
+    // const loading = setInterval(fnLoading, interval());
+    // return loading;
     let progress = 0;
     const fnLoading = () => {
-        $('.progress-bar').style.width = `${progress}%`;
+        const interval = Math.random() * 1000;
+        const step = Math.floor(Math.random() * 11) + 10;
+        $('.progress-bar').css('width', `${progress}%`);
         $('.progress span').text(`${progress}%`);
+        progress += step;
+        const fnTime = setTimeout(fnLoading, interval);
         const fnLoaded = () => {
-            clearInterval(loading);
-            progress = 100;
+            console.log(progress);
+            clearTimeout(fnTime);
+            $('.progress-bar').css('width', '100%');
+            $('.progress span').text('100%');
             return setTimeout(() => {
+                $('.progress span').addClass('text-warning bg-info px-2');
+                $('.progress span').css('font-size', '1rem');
                 $('.progress span').text('Click either tab to start');
-            }, 3000)
-        }
-        progress >= 100 ? fnLoaded() : progress += step;
-    } 
+            }, 3000);
+        };
+        // console.log(progress);
+    };
+
+    // progress >= 100 ? fnLoaded() : fnLoading();
+
 })
