e5d8469a4df574e884c2cb51710b7c804811c1c5
legenddaniel <siyuanincanada@gmail.com>
2020-01-31 22:30:33 -0500

    Update

--- cash-register-new.js
+++ cash-register-new.js
@@ -1,7 +1,6 @@
 const currency = {
     // For simpler calculation we bring penny which has been discontinued back here
-    // Also notice this is in a global scope
-    
+
     'PENNY': 0.01,
     'NICKEL': 0.05,
     'DIME': 0.1,
@@ -12,21 +11,25 @@ const currency = {
     'TEN': 10,
     'TWENTY': 20,
     'FIFTY': 50
-}
-const checkCashRegister = (price, cash, cid) => {
+};
 
-    let [cacheChange, changeDue, change] = [cid, cash - price, []];
-    cid.reverse();
+(() => {
+    return checkCashRegister = (price, cash, cid) => {
+        // greedy algorithm
 
-    // First we count how many we need for this face value of cash, then take all/part of a specific face value of cash out of the cid and set it as the change, finally calculate how much left for the calculation of next face value of cash. *100/100 for rounding issue
-    for (let unit = 0; unit < cid.length; unit++) {
-        const need = Math.floor(changeDue / currency[cid[unit][0]]);
-        cacheChange[unit][1] = Math.min(need, cid[unit][1]);
-        changeDue = Math.round((changeDue - cacheChange[unit][1] * currency[cid[unit][0]]) * 100) / 100;
-    }
+        let [cacheChange, changeDue, change] = [cid, cash - price, []];
+        cid.reverse();
 
-    return (
-        change = changeDue ? [] : cacheChange,
-        change.reverse().flat().filter(i => i - 0 >= 0)
-    );
-}
+        // First we count how many we need for this face value of cash, then take all/part of a specific face value of cash out of the cid and set it as the change, finally calculate how much left for the calculation of next face value of cash. *100/100 for rounding issue
+        for (let unit = 0; unit < cid.length; unit++) {
+            const need = Math.floor(changeDue / currency[cid[unit][0]]);
+            cacheChange[unit][1] = Math.min(need, cid[unit][1]);
+            changeDue = Math.round((changeDue - cacheChange[unit][1] * currency[cid[unit][0]]) * 100) / 100;
+        }
+
+        return (
+            change = changeDue ? [] : cacheChange,
+            change.reverse().flat().filter(i => +i >= 0)
+        );
+    };
+}) ();
