6985f82e5b87e4da0f9c1ae504c185f1b1b4c1e7
legenddaniel <siyuanincanada@gmail.com>
2020-02-23 23:45:12 -0500

    Update browser.js

--- src/basic/browser.js
+++ src/basic/browser.js
@@ -49,8 +49,7 @@ $(() => {
                     }
                 })(),
             });
-        }
-        for (let browser in browsers) {
+
             if (browsers[browser].compatibility) {
                 return true;
             }
