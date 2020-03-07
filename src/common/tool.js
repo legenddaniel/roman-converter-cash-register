f7923d590906d3928f8ed16819a3eea5e6c3862b
legenddaniel <siyuanincanada@gmail.com>
2020-03-06 21:07:54 -0500

    Update tool.js

--- src/common/tool.js
+++ src/common/tool.js
@@ -2,42 +2,44 @@
 
 $(() => {
 
-    class Popover {
-        constructor(placement, html, title, content) {
-            this.placement = placement;
-            this.html = html;
-            this.title = title;
-            this.content = content;
-        }
-    }
-
     const setPopover = (selector, parameter) => {
         selector.popover(parameter);
     };
     const $history = $('.clear-history');
     const $score = $('.btn-score');
+    const popoverHistory = {
+        placement: 'bottom',
+        html: true,
+        title: 'You are cleaning the history',
+        content: `
+        <div>
+            <span>All records will be removed &nbsp;&nbsp;</span>
+            <a class="confirm-clear-history btn btn-sm btn-danger">Yes</a>
+            <a class="btn btn-sm btn-link">No</a>
+        </div>
+    `
+    };
+    const popoverScore = {
+        placement: 'bottom',
+        html: true,
+        title: 'You are resetting the score',
+        content: () => {
+            return (
+                `<div>
+                    <span>Score will be restored to 0 &nbsp;&nbsp;</span>
+                    <a class="confirm-clear-score btn btn-sm btn-danger">Yes</a>
+                    <a class="btn btn-sm btn-link">No</a>
+                </div>
+                <p class="current-score">${methodsMain.setScoreHtml()}</p>
+                `
+            );
+        },
+    };
 
-    const popoverContentHistory = `
-    <div>
-        <span>All records will be removed &nbsp;&nbsp;</span>
-        <a class="confirm-clear-history btn btn-sm btn-danger">Yes</a>
-        <a class="btn btn-sm btn-link">No</a>
-    </div>
-`;
-    const popoverContentScore = () => {
-        return (
-            `<div>
-                <span>Score will be restored to 0 &nbsp;&nbsp;</span>
-                <a class="confirm-clear-score btn btn-sm btn-danger">Yes</a>
-                <a class="btn btn-sm btn-link">No</a>
-            </div>
-            <p class="current-score">${methodsMain.setScoreHtml()}</p>
-            `
-        );
-    }
+    $('.btn-score span').text(`Score: ${methodsMain.getProp('scoreCash', 'score')}%`);
+    
+    $('.eye, .btn-history button').tooltip();
 
-    const popoverHistory = new Popover('bottom', true, 'You are cleaning the history', popoverContentHistory);
-    const popoverScore = new Popover('bottom', true, 'You are resetting the score', popoverContentScore());
     setPopover($history, popoverHistory);
     setPopover($score, popoverScore);
 
@@ -47,10 +49,6 @@ $(() => {
             setPopover($score, 'hide');
         })
     })
-
-    $('.btn-score span').text(`Score: ${methodsMain.getProp('scoreCash', 'score')}%`);
-
-    $('.eye, .btn-history button').tooltip();
 
     $('.mobile').on('click', e => {
         $('nav ul').toggle();
